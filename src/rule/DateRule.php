54bf3c357b69ed70ca2227cd0e5ed2dbc017a054
Vitaly Demyanenko <vitaly_demyanenko@yahoo.com>
2026-06-14 09:47:11 +0300

    preparing for release - fix

---
+++ src/rule/DateRule.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace queasy\validation;
+
+class DateRule extends AbstractRule
+{
+    public function validate($value)
+    {
+        return empty($value)
+            || preg_match('~^([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$~', $value)
+            || preg_match('~^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/[0-9]{4}$~', $value)
+            || preg_match('~^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/[0-9]{4}$~', $value);
+
+    }
+
+    public function message()
+    {
+        return 'This field should be a correct date';
+    }
+}
+
